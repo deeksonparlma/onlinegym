c7c4b10a1b61f6786eafcec0f18d34b98825718a
dicksonparlma <dicksonparlma@gmail.com>
2019-06-01 17:50:16 +0300

    added the input types and setup the authentication system

--- app/src/main/java/com/epicodus/gym/Home.java
+++ app/src/main/java/com/epicodus/gym/Home.java
@@ -1,9 +1,11 @@
 package com.epicodus.gym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -11,17 +13,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.email) EditText mEmail;
     @BindView(R.id.password) EditText mPassword;
     @BindView(R.id.login) Button mLogin;
+    private FirebaseAuth mAuth;
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         mLogin.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -34,6 +45,24 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
          }
          else if(password.isEmpty() || password.length() < 7){
              mPassword.setError("password is short / empty");
+         }
+         else if(!password.isEmpty() && !email.isEmpty()){
+
+             mAuth.signInWithEmailAndPassword(email, password)
+                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                         @Override
+                         public void onComplete(@NonNull Task<AuthResult> task) {
+                             if (task.isSuccessful()) {
+                                 Intent intent = new  Intent(Home.this,MainActivity.class);
+                                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                 startActivityForResult(intent, 0);
+                                 finish();
+                             } else {
+                                 Toast.makeText(Home.this, "Login failed.",
+                                         Toast.LENGTH_SHORT).show();
+                             }
+                         }
+                     });
          }
         }
     }
