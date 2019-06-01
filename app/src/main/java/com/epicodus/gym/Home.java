f85ce31b87d8df16f6b8a5d383d217b970d87138
dicksonparlma <dicksonparlma@gmail.com>
2019-06-01 14:07:13 +0300

    used custom font,styled the login xml bit

---
+++ app/src/main/java/com/epicodus/gym/Home.java
@@ -0,0 +1,18 @@
+package com.epicodus.gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/com/epicodus/gym/MainActivity.java
+++ app/src/main/java/com/epicodus/gym/MainActivity.java
@@ -1,14 +1,29 @@
 package com.epicodus.gym;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+@BindView(R.id.start) Button mStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mStart ){
+            Intent intent = new Intent(this,Home.class);
+            startActivity(intent);
+        }
     }
 }
