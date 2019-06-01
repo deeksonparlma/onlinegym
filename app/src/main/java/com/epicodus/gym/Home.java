160dd44f758977729ac756b2ccd0c5ffc36e94f3
dicksonparlma <dicksonparlma@gmail.com>
2019-06-01 17:13:34 +0300

    validated form inputs and ignored google play services

--- app/src/main/java/com/epicodus/gym/Home.java
+++ app/src/main/java/com/epicodus/gym/Home.java
@@ -6,13 +6,35 @@ import butterknife.ButterKnife;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.email) EditText mEmail;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.login) Button mLogin;
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        mLogin.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+    if(v == mLogin){
+         String email = mEmail.getText().toString();
+         String password = mPassword.getText().toString();
+         if(email.isEmpty() || !(Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+             mEmail.setError("invalid email");
+         }
+         else if(password.isEmpty() || password.length() < 7){
+             mPassword.setError("password is short / empty");
+         }
+        }
     }
 }
